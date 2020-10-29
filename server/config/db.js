c6d4b6cfb4cdebb8ff68701aa71e415ac853a141
RHalai-Engin <Rajkumarhalai0@gmail.com>
2020-10-28 21:25:07 -0500

    edited server.json

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book_list"
+  //"URI": "mongodb://localhost/book_list"
+  "URI":"mongodb+srv://Rajan:GDxqw9uKgU9IlMJ4@mongodbserver.hum9j.mongodb.net/book_list?retryWrites=true&w=majority"
 };
